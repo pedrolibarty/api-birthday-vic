f00491a835a9ce2cadae340607ce043e4d5f2ee5
pedrolibarty <pedrolibarty@outlook.com>
2025-04-05 10:40:48 -0300

    feat: bug

--- api/listCompanions.js
+++ api/listCompanions.js
@@ -1,26 +1,38 @@
 import db from "./data-source.js";
 
-export default async function handler(req, res){
+export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
   if (req.method === "OPTIONS") return res.status(200).end();
 
-  if (req.method !== "GET") {
+  if (req.method !== "POST") {
     return res.status(405).json({ message: "Método não permitido" });
   }
 
-  const { id_guest } = req.query;
+  const { id_guest } = req.body;
 
-  console.log(req.query)
+  if (!id_guest) {
+    return res
+      .status(400)
+      .json({ error: "Parâmetro 'id_guest' é obrigatório." });
+  }
 
-  const { data, error } = await db
-    .from("companion")
-    .select("*")
-    .eq("cod_guest", id_guest);
+  try {
+    const { data, error } = await db
+      .from("companion")
+      .select("*")
+      .eq("cod_guest", id_guest);
 
-  if (error) return res.status(500).json({ error: error.message });
+    if (error) {
+      return res.status(500).json({ error: error.message });
+    }
 
-  return res.status(200).json(data);
-  };
+    return res.status(200).json(data);
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ error: "Erro interno do servidor", details: err.message });
+  }
+}
